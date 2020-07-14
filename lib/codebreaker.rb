770e4a5c985dc0a62b930bea0517cdce711fadb4
Konstantin <konstantin.faleev@gmail.com>
2020-07-15 01:54:44 +0300

    added motivation

--- lib/codebreaker.rb
+++ lib/codebreaker.rb
@@ -3,6 +3,7 @@ require_relative 'codebreaker/game_constant'
 require_relative 'codebreaker/score'
 require_relative 'codebreaker/storage'
 require_relative 'codebreaker/user_score'
+require_relative 'codebreaker/motivation'
 
 module Codebreaker
   class Error < StandardError; end
---
+++ lib/codebreaker/motivation.rb
@@ -0,0 +1,15 @@
+module Codebreaker
+  module Motivation
+    private
+
+    def motivation_message(msg)
+      return unless message_is_allowed?
+
+      msg
+    end
+
+    def message_is_allowed?
+      !game.won? && game.attempts == rand(1..game.configuration.max_attempts)
+    end
+  end
+end
