d317ff38791afe0dd2c8c0e6ddf19eee825e1473
Konstantin <konstantin.faleev@gmail.com>
2020-07-15 01:47:36 +0300

    added storage

--- lib/codebreaker.rb
+++ lib/codebreaker.rb
@@ -1,6 +1,7 @@
 require 'codebreaker/version'
 require_relative 'codebreaker/game_constant'
 require_relative 'codebreaker/score'
+require_relative 'codebreaker/storage'
 
 module Codebreaker
   class Error < StandardError; end
