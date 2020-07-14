327ead7a10c3adb1a18878283c720d459d271c5d
Konstantin <konstantin.faleev@gmail.com>
2020-07-15 01:49:58 +0300

    added user score

--- lib/codebreaker.rb
+++ lib/codebreaker.rb
@@ -2,6 +2,7 @@ require 'codebreaker/version'
 require_relative 'codebreaker/game_constant'
 require_relative 'codebreaker/score'
 require_relative 'codebreaker/storage'
+require_relative 'codebreaker/user_score'
 
 module Codebreaker
   class Error < StandardError; end
