31ba57ad57622e3c598f2976400af9b61b18f0c5
Konstantin <konstantin.faleev@gmail.com>
2020-07-16 20:20:38 +0300

    added message and locale

--- lib/codebreaker.rb
+++ lib/codebreaker.rb
@@ -1,12 +1,9 @@
-require 'codebreaker/version'
+require_relative 'codebreaker/version'
 require_relative 'codebreaker/game_constant'
 require_relative 'codebreaker/score'
 require_relative 'codebreaker/storage'
 require_relative 'codebreaker/user_score'
 require_relative 'codebreaker/motivation'
 require_relative 'codebreaker/game'
-
-module Codebreaker
-  class Error < StandardError; end
-  # Your code goes here...
-end
+require_relative 'codebreaker/localization'
+require_relative 'codebreaker/message'
